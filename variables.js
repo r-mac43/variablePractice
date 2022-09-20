8e035cab4bb6e007697d4976a9a52e0626921a3f
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-09-20 15:02:19 -0700

    Print out variables to the terminal

--- variables.js
+++ variables.js
@@ -7,3 +7,17 @@ var favoriteNumber = 8;
 
 var test = 6 < 3;
 var testAgain = 2 < 3;
+
+name = 'George';
+petDog = 'Zepplin';
+favoriteFood = 'tacos';
+
+age = '35';
+favoriteNumber = '17';
+
+test = 1 < 3;
+
+console.log(name);
+console.log(age);
+console.log(petDog);
+console.log(test);
